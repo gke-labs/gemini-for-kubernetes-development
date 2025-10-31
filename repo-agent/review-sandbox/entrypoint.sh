a1025e78f9398ce911947e4b6152640414870642
Barni Seetharaman <barni@google.com>
2025-10-31 19:18:24 +0000

    fixes for demo.
    1. apply all repowatches
    2. fix: reviewagent entrypoint to change the location of gemini output txt to conform to sidecar changes
    3. generated rgd files updated
    4. update examples

--- repo-agent/review-sandbox/entrypoint.sh
+++ repo-agent/review-sandbox/entrypoint.sh
@@ -17,12 +17,12 @@ export GEMINI_API_KEY=`cat /tokens/gemini`
 set -x
 # protection against running gemini on an unpause
 # if gemini-promt.txt dont run gemini else create and run it
-if [ -f gemini-prompt.txt ]; then
+if [ -f ../gemini-prompt.txt ]; then
   echo "gemini-prompt.txt exists, skipping gemini generation"
 else
   echo "gemini-prompt.txt does not exist, running gemini"
-  echo "$AGENT_PROMPT" > gemini-prompt.txt
-  gemini -y  -p  "$AGENT_PROMPT" > gemini-output.txt || true
+  echo "$AGENT_PROMPT" > ../gemini-prompt.txt
+  gemini -y  -p  "$AGENT_PROMPT" > ../gemini-output.txt || true
 fi
 #/usr/local/bin/code-server-entrypoint
 /usr/bin/code-server --auth=none --bind-addr=0.0.0.0:13337
