e82ce4b8f9decc627b69b5d52e2d8fb6a07440ca
Janet Kuo <chiachenk@google.com>
2025-11-19 16:51:51 -0800

    Make component versions configurable

--- repo-agent/installer.sh
+++ repo-agent/installer.sh
@@ -7,6 +7,10 @@ echo "Checking params"
 : "${GEMINI_API_KEY:?Error: GEMINI_API_KEY is not set. Please set it before running this script.}"
 : "${GITHUB_PAT:?Error: GITHUB_PAT is not set. Please set it before running this script.}"
 : ${NAMESPACE:=default}
+: ${ENVOY_GW_VERSION:=v1.5.2}
+: ${KRO_VERSION:=0.5.1}
+: ${AGENT_SANDBOX_VERSION:=v0.1.0}
+: ${REPO_AGENT_VERSION:=v0.1.0-rc.2}
 
 echo "Getting git config..."
 GIT_USER_NAME=$(git config --global user.name)
@@ -27,19 +31,18 @@ command -v helm >/dev/null 2>&1 || { echo >&2 "helm not found. Please install it
 echo "All prerequisites are installed."
 
 echo "Installing Envoy GW"
-helm install eg oci://docker.io/envoyproxy/gateway-helm --version v1.5.2 -n envoy-gateway-system --create-namespace
+helm install eg oci://docker.io/envoyproxy/gateway-helm --version ${ENVOY_GW_VERSION} -n envoy-gateway-system --create-namespace
 sleep 5
 kubectl wait --timeout=5m -n envoy-gateway-system deployment/envoy-gateway --for=condition=Available
 
 echo "Installing KRO"
-KRO_VERSION=0.5.1
 helm install kro oci://registry.k8s.io/kro/charts/kro --namespace kro --create-namespace --version=${KRO_VERSION}
 helm -n kro list
 sleep 5
 kubectl get pods -n kro
 
 echo "Installing Sandbox"
-kubectl apply -f https://github.com/kubernetes-sigs/agent-sandbox/releases/download/v0.1.0-rc.0/manifest.yaml
+kubectl apply -f https://github.com/kubernetes-sigs/agent-sandbox/releases/download/${AGENT_SANDBOX_VERSION}/manifest.yaml
 
 # Create namespace if it doesn't exist
 kubectl get namespace ${NAMESPACE} >/dev/null 2>&1 || kubectl create namespace ${NAMESPACE}
@@ -49,7 +52,6 @@ kubectl create secret -n ${NAMESPACE} generic gemini-vscode-tokens --from-litera
 kubectl create secret -n ${NAMESPACE} generic github-pat --from-literal=pat=${GITHUB_PAT} --from-literal=name="`git config --global user.name`" --from-literal=email=`git config --global user.email`
 
 echo "Install repo agent"
-: ${REPO_AGENT_VERSION:=v0.1.0-rc.2}
 kubectl apply -f https://github.com/gke-labs/gemini-for-kubernetes-development/releases/download/${REPO_AGENT_VERSION}/manifest.yaml
 
 echo "Setting up repo-agent for namespace ${NAMESPACE}"
