7f875e3d7570f6b3ee411895bacd3c54e02b3f9f
Justin SB <justinsb@google.com>
2025-11-25 13:47:53 +0000

    dev-sandbox: initial support for dotfiles repo

--- repo-agent/dev-sandbox/main.go
+++ repo-agent/dev-sandbox/main.go
@@ -6,13 +6,20 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"os/signal"
+	"path/filepath"
 	"strings"
+	"syscall"
 
 	"k8s.io/klog/v2"
 )
 
 func main() {
 	ctx := context.Background()
+
+	// Listen to signals so we can gracefully shutdown
+	ctx, stopListeningToSignals := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	defer stopListeningToSignals() // Ensure we stop listening to signals.
 
 	if err := run(ctx); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
@@ -24,6 +31,14 @@ func main() {
 
 func run(ctx context.Context) error {
 	log := klog.FromContext(ctx)
+
+	var b ImageBuilder
+	if dotFilesRepo := os.Getenv("USER_DOTFILESREPO"); dotFilesRepo != "" {
+		if err := b.InstallDotfilesRepo(ctx, dotFilesRepo); err != nil {
+			// Note: we don't fail the entire startup if dotfiles installation fails
+			log.Error(err, "installing dotfiles repo", "repo", dotFilesRepo)
+		}
+	}
 
 	cmdCodeSrv, err := startCodeServer(ctx)
 	if err != nil {
@@ -63,4 +78,82 @@ func startCodeServer(ctx context.Context) (*exec.Cmd, error) {
 	}
 	log.Printf("Running code-server in subprocess %d\n", cmd.Process.Pid)
 	return cmd, nil
+}
+
+// ImageBuilder is responsible for initializing the container, e.g. installing dotfiles
+type ImageBuilder struct {
+}
+
+// InstallDotfilesRepo clones and install a dotfiles repo
+func (b *ImageBuilder) InstallDotfilesRepo(ctx context.Context, dotFilesRepo string) error {
+	log := klog.FromContext(ctx)
+
+	// Get the user's cache directory
+	cacheDir, err := os.UserCacheDir()
+	if err != nil {
+		return fmt.Errorf("getting user cache dir: %w", err)
+	}
+
+	dotfilesDir := filepath.Join(cacheDir, "dev-sandbox", "dotfiles")
+
+	if err := b.GitClone(ctx, dotFilesRepo, dotfilesDir); err != nil {
+		return err
+	}
+
+	// Well-known entrypoints
+	entrypoints := []string{
+		"setup",
+	}
+
+	var foundEntrypoint string
+	for _, entrypoint := range entrypoints {
+		p := filepath.Join(dotfilesDir, entrypoint)
+		if _, err := os.Stat(p); err != nil {
+			if !os.IsNotExist(err) {
+				log.Error(err, "error checking for entrypoint", "entrypoint", p)
+			}
+			continue
+		}
+		foundEntrypoint = p
+		break
+	}
+
+	if foundEntrypoint == "" {
+		return fmt.Errorf("unable to find entrypoint in dotfiles repo %q", dotFilesRepo)
+	}
+
+	cmd := exec.CommandContext(ctx, foundEntrypoint)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("error running dotfiles entrypoint %q from repo %q: %w", strings.Join(cmd.Args, " "), dotFilesRepo, err)
+	}
+
+	return nil
+}
+
+// GitClone clones a git repo to the dest directory.
+func (b *ImageBuilder) GitClone(ctx context.Context, source string, dest string) error {
+	log := klog.FromContext(ctx)
+
+	args := []string{
+		"git",
+		"clone",
+		source,
+		dest,
+	}
+
+	cmdString := strings.Join(args, " ")
+	log.Info("cloning git repo", "source", source, "command", cmdString)
+
+	cmd := exec.CommandContext(ctx, args[0], args[1:]...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("cloning git repo %q with %q: %w", source, cmdString, err)
+	}
+
+	return nil
 }
