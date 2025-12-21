38df3e9a27ab85b05ee1b3c3dbb9db3c22b2cc30
codebot-robot <codebot-robot@google.com>
2025-12-21 15:13:54 +0000

    refactor: Move dev-sandbox commands to shared pkg
    Moved to enable easier reuse. Updated import paths accordingly.

--- repo-agent/dev-sandbox/main.go
+++ repo-agent/dev-sandbox/main.go
@@ -15,7 +15,7 @@ import (
 	"github.com/spf13/cobra"
 	"k8s.io/klog/v2"
 
-	"github.com/gke-labs/gemini-for-kubernetes-development/repo-agent/dev-sandbox/pkg/commands"
+	"github.com/gke-labs/gemini-for-kubernetes-development/repo-agent/pkg/commands"
 )
 
 func main() {
