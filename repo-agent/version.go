c74e6e18139e14501efd40baefeb6211f3554a1a
codebot-robot <codebot-robot@google.com>
2026-02-07 04:14:41 +0000

    Expose version and git commit in repo-agent web app
    Fixes #426

--- repo-agent/version.go
+++ repo-agent/version.go
@@ -5,3 +5,9 @@ const (
 	//nolint:revive,var-naming
 	REPOAGENT_RELEASE_VERSION = "v0.1.0-rc.3"
 )
+
+var (
+	// GitCommit is the git commit hash of the binary.
+	// It is set by the build tooling.
+	GitCommit = "unknown"
+)
