950945f404cd9bef5404023c49570c2749511878
codebot-robot <codebot-robot@google.com>
2026-02-19 02:50:29 +0000

    Refactor agentsandboxes CLI and setup workspace
    Refactored the agentsandboxes CLI to follow the options pattern.
    Logic has been moved to agentsandboxes/pkg/commands.
    The agentsandboxes directory is now a separate Go module and is included in go.work.
    
    Fixes #621

---
+++ agentsandboxes/pkg/commands/create.go
@@ -0,0 +1,76 @@
+/*
+Copyright 2026 The Gemini Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package commands
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/gke-labs/gemini-for-kubernetes-development/agentsandboxes"
+	"github.com/spf13/cobra"
+)
+
+// CreateOptions holds options for the create command.
+type CreateOptions struct {
+	Name  string
+	Image string
+}
+
+// InitDefaults initializes default values for CreateOptions.
+func (o *CreateOptions) InitDefaults() {
+	o.Image = "gcr.io/justinsb-knotai-dev/generic-golang:latest"
+}
+
+// BuildCreateCommand builds the cobra command for creating an agent sandbox.
+func BuildCreateCommand() *cobra.Command {
+	var opt CreateOptions
+	opt.InitDefaults()
+
+	cmd := &cobra.Command{
+		Use:   "create [name]",
+		Short: "Create an agent sandbox",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			opt.Name = args[0]
+			return RunCreate(cmd.Context(), opt)
+		},
+	}
+	cmd.Flags().StringVar(&opt.Image, "image", opt.Image, "Container image for the sandbox")
+
+	return cmd
+}
+
+// RunCreate executes the create logic.
+func RunCreate(ctx context.Context, opt CreateOptions) error {
+	client, err := agentsandboxes.NewClient()
+	if err != nil {
+		return err
+	}
+	builder := client.New(opt.Name)
+
+	if opt.Image != "" {
+		builder.Image(opt.Image)
+	}
+
+	sandbox, err := builder.Create(ctx)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Created sandbox: %s/%s\n", sandbox.Namespace, sandbox.Name)
+	return nil
+}
---
+++ agentsandboxes/pkg/commands/delete.go
@@ -0,0 +1,66 @@
+/*
+Copyright 2026 The Gemini Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package commands
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/gke-labs/gemini-for-kubernetes-development/agentsandboxes"
+	"github.com/spf13/cobra"
+)
+
+// DeleteOptions holds options for the delete command.
+type DeleteOptions struct {
+	Name string
+}
+
+// InitDefaults initializes default values for DeleteOptions.
+func (o *DeleteOptions) InitDefaults() {
+}
+
+// BuildDeleteCommand builds the cobra command for deleting an agent sandbox.
+func BuildDeleteCommand() *cobra.Command {
+	var opt DeleteOptions
+	opt.InitDefaults()
+
+	cmd := &cobra.Command{
+		Use:   "delete [name]",
+		Short: "Delete an agent sandbox",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			opt.Name = args[0]
+			return RunDelete(cmd.Context(), opt)
+		},
+	}
+	return cmd
+}
+
+// RunDelete executes the delete logic.
+func RunDelete(ctx context.Context, opt DeleteOptions) error {
+	client, err := agentsandboxes.NewClient()
+	if err != nil {
+		return err
+	}
+
+	if err := client.Delete(ctx, opt.Name); err != nil {
+		return err
+	}
+
+	fmt.Printf("Deleted sandbox: %s\n", opt.Name)
+	return nil
+}
