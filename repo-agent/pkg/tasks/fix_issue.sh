a9fd6e374394fd0fb66c4468a6b19ec43690a2da
Barni Seetharaman <barni@google.com>
2026-05-09 22:39:17 +0000

    Add GIT_SSL_CAINFO to gh wrapper scripts

--- repo-agent/pkg/tasks/fix_issue.sh
+++ repo-agent/pkg/tasks/fix_issue.sh
@@ -7,7 +7,7 @@ if [ ! -f /usr/local/bin/gh ]; then
     echo "creating gh wrapper script"
     cat <<'EOF' > /usr/local/bin/gh
 #!/bin/bash
-HTTPS_PROXY=http://github-portal.overseer-system.svc.cluster.local:80 SSL_CERT_FILE=/etc/github-portal/ca/tls.crt /usr/bin/gh "$@"
+HTTPS_PROXY=http://github-portal.overseer-system.svc.cluster.local:80 SSL_CERT_FILE=/etc/github-portal/ca/tls.crt GIT_SSL_CAINFO=/etc/github-portal/ca/tls.crt /usr/bin/gh "$@"
 EOF
     chmod +x /usr/local/bin/gh
 fi
