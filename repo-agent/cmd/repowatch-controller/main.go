9c476f559e835e8311a2ba148ea4929b6ae84aa7
Barni Seetharaman <barni@google.com>
2026-09-18 21:05:41 -0700

    goimports: stdlib/external import grouping in controller main

--- repo-agent/cmd/repowatch-controller/main.go
+++ repo-agent/cmd/repowatch-controller/main.go
@@ -21,8 +21,9 @@ package main
 
 import (
 	"flag"
-	"k8s.io/klog/v2"
 	"os"
+
+	"k8s.io/klog/v2"
 
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
